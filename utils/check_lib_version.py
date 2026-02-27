3c1c9cbf3db9711fda4d43dd16619e3be010ce92
bang.bui <bang.bui@mservice.com.vn>
2026-02-27 14:33:48 +0700

    update requirement

--- utils/check_lib_version.py
+++ utils/check_lib_version.py
@@ -9,6 +9,8 @@ import scipy as sp # 8
 import tqdm # 9
 import peft # 10
 import transformers as tr # 11
+import cpuinfo
+import pydantic
 import deepspeed as ds # 12
 import sentencepiece as spm # 13
 
@@ -23,5 +25,7 @@ print("8_scipy_version_1.12.0:", sp.__version__)
 print("9_tqdm_version_4.65.0:", tqdm.__version__)
 print("10_peft_version_0.4.0:", peft.__version__)
 print("11_transformers_version_4.31.0:", tr.__version__)
-print("12_deepspeed_version_0.14.0:", ds.__version__)
-print("13_sentencepiece_version_0.2.0:", spm.__version__)
+print("12_cpuinfo_version_8.0.0:", cpuinfo.__version__)
+print("13_pydantic_version_1.11.0:", pydantic.__version__)
+print("14_deepspeed_version_0.14.0:", ds.__version__)
+print("15_sentencepiece_version_0.2.0:", spm.__version__)
